e107066b1f5d4c527d0b657faf5b8b0c76fb8cc3
Thomas Shannon <thomasshannon1117@gmail.com>
2020-07-30 10:28:02 -0700

    Updates

---
+++ precache-manifest.deaa2c912e72f90da4fc017865c30cc9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df23acc5a7dff3bcf9962713561c4927",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9bca4a1bd99723167e17",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "86016800bdded6ba2e48",
+    "url": "/static/js/2.77d68d85.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.77d68d85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9bca4a1bd99723167e17",
+    "url": "/static/js/main.66780bac.chunk.js"
+  },
+  {
+    "revision": "dbf65702745a6d347b70",
+    "url": "/static/js/runtime-main.9263c675.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
